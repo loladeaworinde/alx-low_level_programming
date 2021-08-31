bf618eee9f033921f634dd8799477300b8277415
loladeaworinde <loladeaworinde@yahoo.com>
2021-08-31 12:04:40 -0700

    holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        char *c="Holberton";
+	int i;
+	for(i=0; i<10; i++){
+	        _putchar(c[i]);
+	}
+	putchar('\n);
+	return (0)
+}
